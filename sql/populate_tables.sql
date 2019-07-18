47bd059367d3ad8d5482952b1e466a5070219237
Choy Jin Xiang <choy0062@e.ntu.edu.sg>
2019-07-18 12:43:39 +0800

    Add populate_tables sql file

---
+++ sql/populate_tables.sql
@@ -0,0 +1,13 @@
+INSERT INTO mydb.student (emailStudent)
+VALUES ('studentagnes@gmail.com'),
+('studentbob@gmail.com'),
+('studentkelly@gmail.com'),
+('studentmark@gmail.com'),
+('studentjon@gmail.com'),
+('studentsally@gmail.com'),
+('studentmary@gmail.com');
+
+INSERT INTO mydb.teacher (emailTeacher)
+VALUES ('teacherken@gmail.com'),
+('teacherjoe@gmail.com'),
+('teacherlana@gmail.com');
