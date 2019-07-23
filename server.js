08d7f394fd66662f76e9f30c29455c6fdda7bed1
Choy Jin Xiang <choy0062@e.ntu.edu.sg>
2019-07-23 12:21:09 +0800

    Modify server file for new file structure

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var models = require('./models');
-var routes = require('./routes/index');
-var db_init = require('./database_util/init');
+var models = require('./controllers/models');
+var routes = require('./controllers/routes/index');
+var db_init = require('./controllers/database_util/init');
 
 const app = express();
 
@@ -15,52 +14,22 @@ app.use(bodyParser.urlencoded());
 
 app.use('/api', routes);
 
-
-
 // Connect to db then start listening
 models.sequelize.sync().then(function() {
-    console.log(db_init);
     if (config.init_db)
         db_init.init();
 
-    var server = app.listen(8080, function() {
-		var host = server.address().address;
-		var port = server.address().port;
+    var server = app.listen(config.server_port, function() {
+      var host = server.address().address;
+      var port = server.address().port;
 
-		console.log("Listening on http://%s%s", host, port);
-		console.log("Dir is " + __dirname);
+      console.log(process.env.NODE_ENV);
+      if (process.env.NODE_ENV === 'dev'){
+        console.log("Listening on http://%s%s", host, port);
+        console.log("Dir is " + __dirname);
+      }
 	});
-  });
-
-/**
- *  MIDDLEWARES
- */
-// Middleware to check length of teacher and student emails are within 40 characters
-// var checkEmailLength = function(req, res, next){
-//     let body =  req.body;
-//     let emailTooLong = false;
-    
-//     if ('teacher' in body && body.teacher.length > 40){
-//         emailTooLong = true;
-//     } else if ('student' in body && body.student.length > 40){
-//         emailTooLong = true;
-//     } else if ('students' in body){
-//         for(var student in body.students){
-//             if (student.length > 40){
-//                 emailTooLong = true;
-//             }
-//         }
-//     }
-
-//     if (emailTooLong){
-//         console.log("Emails too long");
-// 		res.status(400).send({message: "Length of emails cannot be longer than 40 characters."});
-//     } else {
-//         next();
-//     }
-
-// }
-
+});
 
 // Export our app for testing purposes
 module.exports = app;
